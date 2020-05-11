c1269dd0a5cc0056f5d477025831f9e566ccc460
HACKER1xp <64299563+HACKER1xp@users.noreply.github.com>
2020-05-11 16:20:01 +0500

    Add files via upload

--- Hack.py
+++ Hack.py
@@ -350,7 +350,7 @@ def pilih_super():
 	print "\n\033[1;93m ❈           \x1b[1;97mTo Stop Process Press CTRL+Z \033[1;94m    ❈"
 	print "        \033[1;92m●═════════════════◄►════════════════●"
 
-	jalan('              \033[1;97mAahil start cloning Wait...')
+	jalan('              \033[1;97mXPT start cloning Wait...')
 	print  "       \033[1;92m ●════════════════◄►════════════════●" 
 
 	def main(arg):
